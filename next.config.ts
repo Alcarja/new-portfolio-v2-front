f49961be1b5e116d754ae07478198f554f24b81a
Alcaraz <jaime.mediano@gmail.com>
2026-02-25 13:26:34 +0100

    -

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,14 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     domains: ["j10y235gwq.ufs.sh"],
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "116.203.109.157",
+        port: "",
+        pathname: "/uploads/**",
+      },
+    ],
   },
 };
 export default nextConfig;
